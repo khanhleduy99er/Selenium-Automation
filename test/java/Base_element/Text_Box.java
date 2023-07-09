701df351abcd0e8db61dc0057f65d54b1c9de19b
L.D.Khanh <khanhleduy99er>
2023-07-09 19:28:22 +0700

    completed text box TCs

---
+++ test/java/Base_element/Text_Box.java
@@ -0,0 +1,75 @@
+package Base_element;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Text_Box {
+    public WebDriver driver;
+    public JavascriptExecutor javascriptExecutor;
+    public String pageUrl = "https://demoqa.com/text-box";
+    public long timeWait = 3000;
+
+    // element in website
+    String fullNameInput = "khanh le ";
+    String emailAddressInput = "khanhleduy99er@gmail.com";
+    String currentAddressInput = "555b/A2 nhi hoa, thanh pho bien hoa";
+    String permanentAddressInput = "none";
+
+    // locator for input text box
+    By nameTextBox = By.xpath("//input[@id=\"userName\" and @type=\"text\"]");
+    By emailTextBox = By.xpath("//input[@id=\"userEmail\" and @type=\"email\"]");
+    By currentAddressTextBox = By.xpath("//textarea[@id=\"currentAddress\" and @class=\"form-control\"]");
+    By permanentAddressTextBox = By.xpath("//textarea[@id=\"permanentAddress\" and @class=\"form-control\"]");
+    By submitButton = By.xpath("//button[@id=\"submit\" and @type=\"button\"]");
+
+    public void generateDriverBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    // input text box function
+    public void TextBoxWebElement(By locator, String inputTextBoxElement){
+        WebElement element = driver.findElement(locator);
+        element.clear();
+        element.sendKeys(inputTextBoxElement);
+    }
+
+    // click function
+    public void SubmitButtonElement(By locator){
+        WebElement element = driver.findElement(locator);
+        element.submit();
+    }
+
+    @BeforeClass
+    public void BeforeClass(){
+        generateDriverBrowser();
+
+        javascriptExecutor = (JavascriptExecutor) driver;
+
+        driver.manage().window().maximize();
+        driver.get(pageUrl);
+    }
+    @Test
+    public void TC_01_InputIntoTextBox(){
+        TextBoxWebElement(nameTextBox, fullNameInput);
+        TextBoxWebElement(emailTextBox, emailAddressInput);
+        TextBoxWebElement(currentAddressTextBox, currentAddressInput);
+        TextBoxWebElement(permanentAddressTextBox, permanentAddressInput);
+
+        SubmitButtonElement(submitButton);
+    }
+
+
+    @AfterClass
+    public void AfterClass(){
+        driver.quit();
+    }
+
+}
--- test/java/selenium/Login_Guru99.java
+++ test/java/selenium/Login_Guru99.java
@@ -20,7 +20,7 @@ public class Login_Guru99 {
     public String emailInput = "khanhld@gmail.com";
     public String passwordInput = "123456";
     public int timeWait = 100 ;
-    public int timeWaitReload = 7000;
+    public int timeWaitReload = 18000;
     public String beforeUrl ;
     public void generateEdgeDriver() {
         WebDriverManager.edgedriver().setup();
@@ -55,26 +55,24 @@ public class Login_Guru99 {
         // input password
         passwordElement.sendKeys(passwordInput) ;
         // waiting then input email address
-        try {
-            Thread.sleep(timeWait);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
         // click login btn
         loginButtonElement.click();
+
+            try {
+                Thread.sleep(timeWait);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
     }
 
-    //@Test
+    @Test
     public void TCs_CheckSuccessful(){
         // waiting loading website then get url
-        try {
-            Thread.sleep(timeWaitReload);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        String afterUrl = driver.getCurrentUrl();
-        Assert.assertNotEquals(beforeUrl, afterUrl);
+
+        String getStatus = driver.findElement(By.xpath("//div[@class='error-copy']/h3")).getText();
+        String expectStatus = "Successfully Logged in..." ;
+        Assert.assertEquals(getStatus, expectStatus);
     }
 
     @AfterClass
